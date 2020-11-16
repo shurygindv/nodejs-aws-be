3b34c68d2af51b1d654173e04b12bf0bca771bc7
Daniil_Shurygin <shurygindv@ya.ru>
2020-11-16 04:09:42 +0400

    feat: add product api call

--- import-service/src/import-file-parser.ts
+++ import-service/src/import-file-parser.ts
@@ -1,10 +1,13 @@
 import { APIGatewayProxyHandler } from "aws-lambda";
 import AWS from "aws-sdk";
 import csvParser from "csv-parser";
+import request from "request";
 
 import { httpResponse } from "./libs/http";
 
 import "source-map-support/register";
+
+// TODO: 1. refactor this, so much entities in one file
 
 const s3 = new AWS.S3({
   region: "eu-west-1",
@@ -13,7 +16,34 @@ const s3 = new AWS.S3({
 
 const BUCKET_NAME = process.env.BUCKET_NAME;
 
-const logFileWithS3Stream = (s3ObjectKey: string) =>
+// TODO: 2. avoid any
+const addProduct = (product: any) => {
+  const url =
+    "https://qt1ja1wqr9.execute-api.eu-west-1.amazonaws.com/dev/products";
+
+  const form = {
+    title: product.title,
+    description: product.description,
+    price: product.price,
+    count: product.count,
+    imageName: product.imageName,
+  };
+
+  return new Promise((resolve, reject) => {
+    request.post({ url, form }, (err, _r, body) => {
+      if (err) {
+        console.error(err);
+
+        reject(err);
+      }
+
+      resolve(body);
+    });
+  });
+};
+
+// TODO: streams/promises pipeline later
+const addProductWithStreamLogging = (s3ObjectKey: string) =>
   new Promise((resolve, reject) => {
     const streamParams = {
       Bucket: BUCKET_NAME,
@@ -26,9 +56,13 @@ const logFileWithS3Stream = (s3ObjectKey: string) =>
 
     stream
       .pipe(csvParser())
-      .on("data", console.log)
-      .on("error", reject)
-      .on("end", resolve);
+      .on("data", (product) => {
+        console.info(product);
+
+        // temp: only for demo file uploading
+        addProduct(product).then(resolve).catch(reject);
+      })
+      .on("error", reject);
   });
 
 export const importsFileParser: APIGatewayProxyHandler = async (
@@ -43,30 +77,27 @@ export const importsFileParser: APIGatewayProxyHandler = async (
     console.info(record.s3.object);
 
     try {
-      await logFileWithS3Stream(recordObjectKey)
+      await addProductWithStreamLogging(recordObjectKey);
 
       const copyCfg = {
         Bucket: BUCKET_NAME,
         CopySource: `${BUCKET_NAME}/${recordObjectKey}`,
         Key: recordObjectKey.replace("uploaded", "parsed"),
       };
-  
+
       const deleteCfg = {
         Bucket: BUCKET_NAME,
         Key: recordObjectKey,
       };
-  
-      console.info(copyCfg);
-  
+
       await s3.copyObject(copyCfg).promise();
       await s3.deleteObject(deleteCfg).promise();
     } catch (e) {
       console.error(e);
 
-      return httpResponse.failureResult({message: 'something went wrong'})
+      return httpResponse.failureResult({ message: "something went wrong" });
     }
   }
-
 
   return httpResponse.successResult({});
 };
