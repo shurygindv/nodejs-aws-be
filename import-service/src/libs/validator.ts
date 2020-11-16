2e81611237ee52138cba6a2fdaf228ba6be5a577
Daniil_Shurygin <shurygindv@ya.ru>
2020-11-16 20:33:42 +0400

    fix: csv filename validator

--- import-service/src/libs/validator.ts
+++ import-service/src/libs/validator.ts
@@ -1,5 +1,5 @@
 const PNG_OR_JPG_REGEX = /^[a-zA-Z0-9]{3,}\.(jpg|png)$/i;
-const CSV_FILE_NAME_REGEX = /^[a-zA-Z0-9]+\.csv$/i;
+const CSV_FILE_NAME_REGEX = /^[a-zA-Z0-9,.* -=()]+\.csv$/i;
 const GUID_REGEX = /^[0-9A-Fa-f\-]{36}$/;
 
 const isImageFileName = (n: string) => PNG_OR_JPG_REGEX.test(n);
