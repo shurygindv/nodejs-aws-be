9c382ea1b9a73136f60520239c1ff153b021ce6b
Daniil_Shurygin <shurygindv@ya.ru>
2020-11-24 13:59:55 +0400

    fix: sns: filter works

--- product-service/src/lambda/catalog-batch-process.ts
+++ product-service/src/lambda/catalog-batch-process.ts
@@ -7,21 +7,51 @@ import { productService } from "../services/product-service";
 
 const PRODUCT_TOPIC_ARN = process.env.PRODUCT_TOPIC_ARN;
 
-const withParams = product => ({
+type ProductDto = {
+  title: string;
+  description: string;
+  price: number;
+  count: number;
+  imageName?: string;
+};
+
+const MessageSubject = (p: ProductDto) => `Hey! ${p.title} was uploaded`;
+
+const MessageProductAttributes = (product: ProductDto) => ({
+  title: {
+    DataType: "String",
+    StringValue: `${product.title}`,
+  },
+  description: {
+    DataType: "String",
+    StringValue: `${product.description}`,
+  },
+  count: {
+    DataType: "Number",
+    StringValue: `${product.count}`,
+  },
+  price: {
+    DataType: "Number",
+    StringValue: `${product.price}`,
+  },
+});
+
+const withParams = (product: ProductDto) => ({
   Message: JSON.stringify(product),
   TopicArn: PRODUCT_TOPIC_ARN,
-  Subject: `Hey! Somebody has uploaded files`
+  Subject: MessageSubject(product),
+  MessageAttributes: MessageProductAttributes(product)
 });
 
 const handleResult = (e: Error) => {
   if (e) {
-    console.info(">> EMERGENCY <<")
+    console.info(">> EMERGENCY <<");
     console.error(e.message);
     return;
   }
 
   console.info("Successfuly emailed with product");
-}
+};
 
 const notifyBySNSEmailSubscription = async (products: any[]) => {
   const sns = new AWS.SNS();
@@ -30,11 +60,11 @@ const notifyBySNSEmailSubscription = async (products: any[]) => {
     const params = withParams(p);
     console.info(params);
 
-    await sns.publish(params, handleResult);
+    await sns.publish(params, handleResult).promise();
   }
-}
+};
 
-const isEmpty = (products: any[]) => !products.length;
+const isEmpty = (products: ProductDto[]) => !products.length;
 
 export const catalogBatchProcess: APIGatewayProxyHandler = lambda(
   async (event) => {
